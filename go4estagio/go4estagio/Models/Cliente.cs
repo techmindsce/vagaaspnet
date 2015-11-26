792800c9759c6839d2b1df0640a9fa5ecab4c8b0
Caio Mestres Mendes <caiodev@gmail.com>
2015-11-26 18:39:58 -0300

    Crud do cadastro, onde é possivel criar um contato, deleta-lo e atualizar os contatos.

---
+++ go4estagio/go4estagio/Models/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace go4estagio.Models
+{
+    public class Cliente
+    {
+
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Endereco { get; set; }
+        public String Telefone { get; set; }
+        public String Celular { get; set; }
+        public String Email { get; set; }
+        public DateTime Nascimento { get; set; }
+    }
+
+    public class ClienteContext : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
