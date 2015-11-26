5fe2f726ddb197ac0c210e5805e9bab358a390df
Flábio Assunção <flabiofilho@outlook.com>
2015-11-26 00:19:03 -0200

    Final commit. Validações e testes realizados.

--- AgendaDeClientes/App_Start/RouteConfig.cs
+++ AgendaDeClientes/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AgendaDeClientes
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Clientes", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
